d52e63c0f6400faa98892593e84350bf7999fd74
Jovana Matic <jovana.stankovic85@yahoo.com>
2024-02-29 16:24:42 +0100

    Adding home page hero image

--- app.vue
+++ app.vue
@@ -4,6 +4,15 @@
     <header class="sticky z-50 top-0 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md">
       <a class="text-3xl font-mono" href="">cartrader</a>
     </header>
+    <!-- Nav Bar -->
+    <div class="relative h-screen w-100 bg-no-repeat bg-cover bg-bottom bg-[url('https://starrluxurycars.com/wp-content/uploads/2022/03/SLC-Ferrari-sf90-stradale-2021-1.jpg')]">
+      <div class="z-10 absolute w-full h-full flex justify-center items-center">
+        <div class="text-center w-1/2">
+          <h1 class="text-8xl font-serif mb-10 text-white">Find your drive</h1>
+        </div>
+      </div>
+      <div class="z-1 absolute w-full h-full bg-black opacity-50"></div>
+    </div>
   </div>
 </template>
 
