035c94dc99d94065440afae7c8b6cad5c59c1cc0
Jovana Matic <jovana.stankovic85@yahoo.com>
2024-02-29 10:52:18 +0100

    Create nav bar

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
-    <h2>hello</h2>
+    <!-- Nav Bar -->
+    <header class="sticky z-50 top-0 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md">
+      <a class="text-3xl font-mono" href="">cartrader</a>
+    </header>
   </div>
 </template>
+
+<style scoped>
+</style>
