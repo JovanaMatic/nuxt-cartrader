cf59b66c7763a99449ebc1bd5bed409c836a073a
Jovana Matic <jovana.stankovic85@yahoo.com>
2024-02-29 16:33:04 +0100

    Move code to separate html file to work on detail page

--- app.vue
+++ app.vue
@@ -1,25 +1,5 @@
 <template>
-  <div>
-    <!-- Nav Bar -->
-    <header class="sticky z-50 top-0 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md">
-      <a class="text-3xl font-mono" href="">cartrader</a>
-    </header>
-    <!-- Nav Bar -->
-    <div class="relative h-screen w-100 bg-no-repeat bg-cover bg-bottom bg-[url('https://starrluxurycars.com/wp-content/uploads/2022/03/SLC-Ferrari-sf90-stradale-2021-1.jpg')]">
-      <div class="z-10 absolute w-full h-full flex justify-center items-center">
-        <div class="text-center w-1/2">
-          <h1 class="text-8xl font-serif mb-10 text-white">Find your drive</h1>
-          <!-- Home Search Bar -->
-          <div class="font-serif w-[1000px] text-2xl rounded-full bg-white flex justify-between overflow-hidden drop-shadow-2xl mx-auto">
-            <input type="text" class="py-3 w-full px-5 text-2xl rounded-full focus:outline-none" placeholder="Search by city..."/>
-            <button class="bg-sky-500 px-10 text-white">Search</button>
-          </div>
-          <!-- Home Search Bar -->
-        </div>
-      </div>
-      <div class="z-1 absolute w-full h-full bg-black opacity-50"></div>
-    </div>
-  </div>
+  <div></div>
 </template>
 
 <style scoped>
