d786c34ad2bb338231054f9f740142a6152d4b10
Jovana Matic <jovana.stankovic85@yahoo.com>
2024-03-01 23:22:11 +0100

    Add cars element

--- app.vue
+++ app.vue
@@ -1,34 +1,5 @@
 <template>
   <div>
-    <!-- Cars Page -->
-    <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5">
-      <div class="mt-32 flex">
-      <!-- Cars Side Bar -->
-      <div class="shadow border w-64 mr-10 z-30 h-[190px]">
-        <div class="p-5 flex justify-between relative cursor-pointer border-b">
-          <h3>Location</h3>
-          <h3 class="text-blue-400 capitalize">Belgrade</h3>
-          <div class="absolute border shadow left-56 p-5 top-1 bg-white -m-1">
-            <input class="border p-1 rounded"/>
-            <button class="bg-blue-400 w-full mt-2 rounded text-white p-1">Apply</button>
-          </div>
-        </div>
-        <div class="p-5 flex justify-between relative cursor-pointer border-b">
-          <h3>Make</h3>
-          <h3 class="text-blue-400 capitalize">Audi</h3>
-        </div>
-        <div class="p-5 flex justify-between relative cursor-pointer border-b">
-          <h3>Price</h3>
-          <h3 class="text-blue-400 capitalize">50 000</h3>
-        </div>
-      </div>
-      <!-- Cars Side Bar -->
-
-      <!-- Cars Listings -->
-      <!-- Cars Listings -->
-    </div>
-    </div>
-    <!-- Cars Page -->
   </div>
 </template>
 
