6cb7465863edaef771e973bfb31791db277410ee
Jovana Matic <jovana.stankovic85@yahoo.com>
2024-03-02 19:50:31 +0100

    Create error page

---
+++ error.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-col justify-center items-center w-full mt-10">
+    <h1 class="text-2xl">Ooops you found it</h1>
+    <img class="w-[600px] pt-10" src="https://w7.pngwing.com/pngs/677/284/png-transparent-tyrannosaurus-google-chrome-t-rex-runner-dinosaur-dinosaur-game-angle-white.png" alt="T rex"/>
+    <NuxtLink class="text-xl text-blue-500 underline" to="/">Go back</NuxtLink>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
