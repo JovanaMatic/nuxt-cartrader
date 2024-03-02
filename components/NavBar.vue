263543359e75a68a3b39ecb1be12e57b75f9f75c
Jovana Matic <jovana.stankovic85@yahoo.com>
2024-03-02 12:49:37 +0100

    Create pages and components directory

---
+++ components/NavBar.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+<template>
+  <div>
+    <header class="sticky z-50 top-0 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md">
+      <a class="text-3xl font-mono" href="">cartrader</a>
+    </header>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+<template>
+  <div>
+    <NavBar />
+    <CarHero />
+  </div>
+</template>
+<style scoped>
+
+</style>
