65863cd1eb7f421f11b11230891b6ea5c3904616
Jovana Matic <jovana.stankovic85@yahoo.com>
2024-03-02 19:20:17 +0100

    Create default layout

--- pages/city/[city]/car.vue
+++ pages/city/[city]/car.vue
@@ -1,6 +1,5 @@
 <script setup>
   const route = useRoute()
-  console.log(userCarNameFormat(route.params.make))
   useHead({
     title: `${route.params.make ? userCarNameFormat(route.params.make) : 'Cars'} in ${userCarNameFormat(route.params.city)}`
   })
@@ -8,7 +7,6 @@
 
 <template>
   <div>
-    <NavBar />
     <div class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5">
       <div class="mt-32 flex">
       <CarSideBar />
--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,6 @@
 </script>
 <template>
   <div>
-    <NavBar />
     <CarHero />
   </div>
 </template>
